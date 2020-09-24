ad5e069f335f22f5366a5028fb1800b8bee2e7eb
The Great Tao <nmnhat8668@gmail.com>
2020-09-24 13:29:10 +0700

    Update Mapbox

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,5 @@
 Nova.booting((Vue, router, store) => {
-  Vue.component('index-nova-google-maps', require('./components/IndexField'))
-  Vue.component('detail-nova-google-maps', require('./components/DetailField'))
-  Vue.component('form-nova-google-maps', require('./components/FormField'))
-})
+    Vue.component('index-nova-mapbox', require('./components/IndexField'));
+    Vue.component('detail-nova-mapbox', require('./components/DetailField'));
+    Vue.component('form-nova-mapbox', require('./components/FormField'));
+});
--- src/Mapbox.php
+++ src/Mapbox.php
@@ -6,7 +6,7 @@ use Laravel\Nova\Fields\Field;
 
 class Mapbox extends Field
 {
-    public $component = 'nova-mapbox-component';
+    public $component = 'nova-mapbox';
 
     public function accessToken($accessToken)
     {
